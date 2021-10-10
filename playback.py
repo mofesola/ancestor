fa403bbc31987c0fafa796cb932b65ae999ba356
Mofesola Babalola <mofesola@Mofesolas-MBP.speedport.ip>
2021-10-10 22:41:46 +0200

    shift from playsound to pygame

--- playback.py
+++ playback.py
@@ -1,5 +1,6 @@
-from playsound import playsound
+#from playsound import playsound
 import os, random
+import pygame
 
 class Playback:
 
@@ -8,7 +9,12 @@ class Playback:
     
     def playsound():
         print("Playing file " + Playback.get_file() + "...")
-        playsound("sounds/" + Playback.get_file())
+        pygame.mixer.init()
+        pygame.mixer.music.load("sounds/" + Playback.get_file())
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy() == True:
+            continue
+        #playsound("sounds/" + Playback.get_file())
 
 
 if __name__ == "__main__":
