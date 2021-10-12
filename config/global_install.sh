c44d32da01c4e4d5dabd454a3584366c3e070a4f
Mofesola Babalola <mac@Server.local>
2021-10-12 12:05:02 -0500

    update requirements file

--- config/global_install.sh
+++ config/global_install.sh
@@ -1,8 +1,8 @@
 ROOT="/home/pi/ancestor"
 
 sudo apt update
-sudo apt install -y python3-pip libasound-dev portaudio19-dev libportaudio2 libportaudiocpp0 ffmpeg libcairo2-dev python3-gst-1.0
+sudo apt install -y python3-pip libasound-dev portaudio19-dev libportaudio2 libportaudiocpp0 ffmpeg libcairo2-dev python3-gst-1.0 supervisor
 cp $ROOT/config/supervisord.conf /etc/supervisor/supervisord
-pip install --user supervisor
-pip install -r requirements.txt
+pip3 install --user supervisor
+pip3 install -r requirements.txt
 pip install pygobject
