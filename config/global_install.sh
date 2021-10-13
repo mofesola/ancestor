80e6173da35546ddc50774fe9170eb5445a68f3c
Mofesola Babalola <mac@Server.local>
2021-10-13 03:31:41 -0500

    update supervisor config

--- config/global_install.sh
+++ config/global_install.sh
@@ -1,7 +1,9 @@
 ROOT="/home/pi/ancestor"
 
 sudo apt update
-sudo apt install -y python3-pip libasound-dev portaudio19-dev libportaudio2 libportaudiocpp0 ffmpeg libcairo2-dev python3-gst-1.0 supervisor
+sudo apt install -y python3-pip libasound-dev portaudio19-dev libportaudio2 libportaudiocpp0 ffmpeg libcairo2-dev python3-gst-1.0 supervisor libatlas-base-dev
 sudo apt --no-install-recommends install -y jackd2
 cp $ROOT/config/supervisord.conf /etc/supervisor/supervisord.conf
 pip3 install -r requirements.txt
+sudo /etc/init.d/alsa-utils stop
+sudo /etc/init.d/alsa-utils start
