0b584f80c2140a26bedebc61e13ebc138ccf44f9
Mofesola Babalola <mac@Server.local>
2021-11-04 16:23:33 -0500

    add sounds

--- config/global_install.sh
+++ config/global_install.sh
@@ -1,7 +1,7 @@
 ROOT="/home/pi/ancestor"
 
 sudo apt update
-sudo apt install -y python3-pip libasound-dev portaudio19-dev libportaudio2 libportaudiocpp0 ffmpeg libcairo2-dev python3-gst-1.0 supervisor libatlas-base-dev
+sudo apt install -y python3-pip libasound-dev portaudio19-dev libportaudio2 libportaudiocpp0 ffmpeg libcairo2-dev python3-gst-1.0 supervisor libatlas-base-dev git curl libsdl2-mixer-2.0-0 libsdl2-image-2.0-0 libsdl2-2.0-0
 sudo apt --no-install-recommends install -y jackd2
 cp $ROOT/config/supervisord.conf /etc/supervisor/supervisord.conf
 pip3 install -r requirements.txt
